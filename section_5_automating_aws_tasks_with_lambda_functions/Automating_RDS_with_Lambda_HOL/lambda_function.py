a86eff7cfb632f9fed10976f5db9ed7bbcad3b57
Romulo Ferreira Paiva <romulofpaiva@gmail.com>
2024-03-13 08:21:52 -0300

    section 5 - automating rds: implement process_record method

--- section_5_automating_aws_tasks_with_lambda_functions/Automating_RDS_with_Lambda_HOL/lambda_function.py
+++ section_5_automating_aws_tasks_with_lambda_functions/Automating_RDS_with_Lambda_HOL/lambda_function.py
@@ -11,26 +11,56 @@ import csv
 import logging
 
 # constants (database, credentials, currency convertion rates)
+currency_rates = {"USD": 1, 'CAD': 0.79, 'MXN': 0.05}
+database_name = 'rds_hol_db'
+secret_store_arn = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+db_cluster_arn = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # boto3 clients
-s3_client = boto3.client('s3')
+s3_client  = boto3.client('s3')
+rds_client = boto3.client('rds-data')
 
 # configure logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # function to process each record/row from the CSV file
-#def process_record(record):
-    # convert the bill amount to USD using conversion rates
+def process_record(record):
+    # parse record columns into variables
+    id, company_name, country, city, product_line, item, bill_date, currency, bill_amount = record
+    bill_amount = float(bill_amount)
     
-    # if no conversion rate is found for the currency, log an info message
+    # convert the bill amount to USD using conversion rates
+    usd_amount = 0
+    rate = currency_rates.get(currency)
+    if rate:
+        usd_amount = bill_amount * rate
+    else:
+        # if no conversion rate is found for the currency, log an info message
+        logger.warn(f"No conversion rate found for {currency}")
 
     # prepare SQL statement with placeholders for inserting record into the database
+    sql_statement = ("INSERT IGNORE INTO billing_data "
+                        "(id, company_name, country, city, product_line, item, bill_date, currency, bill_amount, bill_amount_usd) "
+                     "VALUES (:id, :company_name, :country, :city, :product_line, :item, :bill_date, :currency, :bill_amount, :usd_amount)")
 
     # prepare parameters for SQL statement
+    sql_parameters = [
+        {'name': 'id', 'value': {'stringValue': id}},
+        {'name': 'company_name', 'value': {'stringValue': company_name}},
+        {'name': 'country', 'value': {'stringValue': country}},
+        {'name': 'city', 'value': {'stringValue': city}},
+        {'name': 'product_line', 'value': {'stringValue': product_line}},
+        {'name': 'item', 'value': {'stringValue': item}},
+        {'name': 'bill_date', 'value': {'stringValue': bill_date}},
+        {'name': 'currency', 'value': {'stringValue': currency}},
+        {'name': 'bill_amount', 'value': {'doubleValue': bill_amount}},
+        {'name': 'usd_amount', 'value': {'doubleValue': usd_amount}}
+    ]
 
     # execute sql statement and log the response
-    
+    response = execute_statement(sql_statement, sql_parameters)
+    logger.info(f"SQL execution response: {response}")
 
 # function to execute SQL statement
 #def execute_sql(sql, params):
@@ -58,7 +88,7 @@ def lambda_handler(event, context):
 
         # process each record in the CSV file
         for record in csv_reader:
-            print(record)
+            process_record(record)
 
         logger.info("Lambda has finished execution.")
     except Exception as e:
